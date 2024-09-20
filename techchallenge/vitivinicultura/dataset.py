83be591cfda12cdf37256996a741ea1c1df6a968
Yuri Braz <ybraz@live.com>
2024-09-20 20:49:05 -0300

    criando o import_dados.py e ajustando os modelos e apis.

--- techchallenge/vitivinicultura/dataset.py
+++ techchallenge/vitivinicultura/dataset.py
@@ -18,14 +18,14 @@ class SanitizadorDataset:
 
         :param config: Dicionário com a configuração do dataset.
         """
-        self.url = config.get('url')
-        self.caminho_saida = config.get('caminho_saida', '.')
-        self.delimitador = config.get('delimitador', ';')
-        self.codificacao = config.get('codificacao', 'utf-8')
-        self.columns_to_drop = config.get('columns_to_drop', [])
-        self.nome_coluna = config.get('nome_coluna')
-        self.coluna_grupo = config.get('coluna_grupo', 'Tipo')
-        self.special_processing = config.get('special_processing')
+        self.url = config.get("url")
+        self.caminho_saida = config.get("caminho_saida", ".")
+        self.delimitador = config.get("delimitador", ";")
+        self.codificacao = config.get("codificacao", "utf-8")
+        self.columns_to_drop = config.get("columns_to_drop", [])
+        self.nome_coluna = config.get("nome_coluna")
+        self.coluna_grupo = config.get("coluna_grupo", "Tipo")
+        self.special_processing = config.get("special_processing")
         self.df = None
 
     def baixar_e_carregar_csv(self):
@@ -42,11 +42,15 @@ class SanitizadorDataset:
 
             # Salvar o arquivo original diretamente no disco
             nome_arquivo_original = os.path.basename(self.url)
-            caminho_arquivo_original = os.path.join(self.caminho_saida, nome_arquivo_original)
+            caminho_arquivo_original = os.path.join(
+                self.caminho_saida, nome_arquivo_original
+            )
 
-            with open(caminho_arquivo_original, 'wb') as f:
+            with open(caminho_arquivo_original, "wb") as f:
                 f.write(resposta.content)
-            logger.info(f"Arquivo original salvo com sucesso em: {caminho_arquivo_original}")
+            logger.info(
+                f"Arquivo original salvo com sucesso em: {caminho_arquivo_original}"
+            )
 
             # Decodificar o conteúdo baixado
             dados_csv = resposta.content.decode(self.codificacao)
@@ -73,7 +77,9 @@ class SanitizadorDataset:
             logger.error(f"Erro ao salvar o arquivo CSV sanitizado: {e}")
             raise
 
-    def salvar_json(self, dados: Union[pd.DataFrame, Dict[Any, Any]], nome_arquivo: str) -> None:
+    def salvar_json(
+        self, dados: Union[pd.DataFrame, Dict[Any, Any]], nome_arquivo: str
+    ) -> None:
         """
         Salva os dados sanitizados em um arquivo JSON.
 
@@ -82,9 +88,9 @@ class SanitizadorDataset:
         """
         try:
             caminho_saida = os.path.join(self.caminho_saida, nome_arquivo)
-            with open(caminho_saida, 'w', encoding='utf-8') as f:
+            with open(caminho_saida, "w", encoding="utf-8") as f:
                 if isinstance(dados, pd.DataFrame):
-                    dados.to_json(f, orient='records', force_ascii=False)
+                    dados.to_json(f, orient="records", force_ascii=False)
                 else:
                     json.dump(dados, f, indent=4, ensure_ascii=False)
             logger.info(f"JSON sanitizado salvo com sucesso em: {caminho_saida}")
@@ -100,22 +106,23 @@ class SanitizadorDataset:
             logger.info("Iniciando sanitização do arquivo")
             if self.columns_to_drop:
                 self.df = self.df.drop(columns=self.columns_to_drop)
-            if self.special_processing == 'importacao_exportacao':
+            if self.special_processing == "importacao_exportacao":
                 self._sanitizar_importacao_exportacao()
             else:
                 df_sanitizado = self._sanitizar_grupos_maiusculos(
-                    nome_coluna=self.nome_coluna,
-                    coluna_grupo=self.coluna_grupo
+                    nome_coluna=self.nome_coluna, coluna_grupo=self.coluna_grupo
                 )
-                if self.special_processing == 'comercio_outros_vinhos':
+                if self.special_processing == "comercio_outros_vinhos":
                     # Processamento especial para 'Comercio'
-                    df_sanitizado['Produto'] = df_sanitizado['Produto'].str.strip()
+                    df_sanitizado["Produto"] = df_sanitizado["Produto"].str.strip()
                     indices_outros = df_sanitizado[
-                        df_sanitizado['Produto'].str.contains("Outros vinhos", case=False, na=False)
+                        df_sanitizado["Produto"].str.contains(
+                            "Outros vinhos", case=False, na=False
+                        )
                     ].index
                     if not indices_outros.empty:
                         indice_inicio = indices_outros[0]
-                        df_sanitizado.loc[indice_inicio:, 'Tipo'] = "OUTROS"
+                        df_sanitizado.loc[indice_inicio:, "Tipo"] = "OUTROS"
                 self.df = df_sanitizado
             logger.info("Sanitização concluída")
             return self.df
@@ -123,7 +130,9 @@ class SanitizadorDataset:
             logger.error(f"Erro durante a sanitização: {e}")
             raise
 
-    def _sanitizar_grupos_maiusculos(self, nome_coluna: str, coluna_grupo: str = 'Tipo') -> pd.DataFrame:
+    def _sanitizar_grupos_maiusculos(
+        self, nome_coluna: str, coluna_grupo: str = "Tipo"
+    ) -> pd.DataFrame:
         """
         Sanitiza um DataFrame agrupando linhas com base em entradas maiúsculas em uma coluna especificada.
 
@@ -149,7 +158,7 @@ class SanitizadorDataset:
         df_sanitizado = self.df[~self.df[nome_coluna].str.isupper()]
         # Reseta o índice e insere a coluna 'id'
         df_sanitizado = df_sanitizado.reset_index(drop=True)
-        df_sanitizado.insert(0, 'id', df_sanitizado.index + 1)
+        df_sanitizado.insert(0, "id", df_sanitizado.index + 1)
         return df_sanitizado
 
     def _sanitizar_importacao_exportacao(self):
@@ -161,27 +170,35 @@ class SanitizadorDataset:
 
             # Obter a lista de anos a partir das colunas
             colunas = self.df.columns
-            colunas_anos = [col for col in colunas if col.startswith(('19', '20')) and not col.endswith('.1')]
+            colunas_anos = [
+                col
+                for col in colunas
+                if col.startswith(("19", "20")) and not col.endswith(".1")
+            ]
 
             for _, linha in self.df.iterrows():
-                pais = linha['País']
+                pais = linha["País"]
                 for coluna_ano in colunas_anos:
                     ano = int(coluna_ano)
                     quantidade = linha.get(coluna_ano, None)
                     valor = linha.get(f"{coluna_ano}.1", None)
 
                     if pd.notnull(quantidade) and pd.notnull(valor):
-                        registros.append({
-                            "País": pais,
-                            "ano": ano,
-                            "quantidade": quantidade,
-                            "valor_usd": valor
-                        })
+                        registros.append(
+                            {
+                                "País": pais,
+                                "ano": ano,
+                                "quantidade": quantidade,
+                                "valor_usd": valor,
+                            }
+                        )
 
             df_sanitizado = pd.DataFrame(registros)
             self.df = df_sanitizado
         except KeyError as e:
-            logger.error(f"Erro: coluna não encontrada. Verifique se a coluna 'País' ou as colunas de anos estão corretas: {e}")
+            logger.error(
+                f"Erro: coluna não encontrada. Verifique se a coluna 'País' ou as colunas de anos estão corretas: {e}"
+            )
             raise
         except Exception as e:
             logger.error(f"Erro durante a sanitização de 'Importacao/Exportacao': {e}")
@@ -191,65 +208,65 @@ class SanitizadorDataset:
 def main():
     # Configuração dos datasets
     datasets = {
-        'producao': {
-            'url': 'http://vitibrasil.cnpuv.embrapa.br/download/Producao.csv',
-            'columns_to_drop': ['control', 'id'],
-            'nome_coluna': 'produto',
-            'coluna_grupo': 'Tipo',
-            'delimitador': ';',
-            'codificacao': 'utf-8',
-            'special_processing': None
+        "producao": {
+            "url": "http://vitibrasil.cnpuv.embrapa.br/download/Producao.csv",
+            "columns_to_drop": ["control", "id"],
+            "nome_coluna": "produto",
+            "coluna_grupo": "Tipo",
+            "delimitador": ";",
+            "codificacao": "utf-8",
+            "special_processing": None,
         },
-        'processamento': {
-            'url': 'http://vitibrasil.cnpuv.embrapa.br/download/ProcessaViniferas.csv',
-            'columns_to_drop': ['control', 'id'],
-            'nome_coluna': 'cultivar',
-            'coluna_grupo': 'Tipo',
-            'delimitador': ';',
-            'codificacao': 'utf-8',
-            'special_processing': None
+        "processamento": {
+            "url": "http://vitibrasil.cnpuv.embrapa.br/download/ProcessaViniferas.csv",
+            "columns_to_drop": ["control", "id"],
+            "nome_coluna": "cultivar",
+            "coluna_grupo": "Tipo",
+            "delimitador": ";",
+            "codificacao": "utf-8",
+            "special_processing": None,
         },
-        'comercio': {
-            'url': 'http://vitibrasil.cnpuv.embrapa.br/download/Comercio.csv',
-            'columns_to_drop': ['control', 'id'],
-            'nome_coluna': 'Produto',
-            'coluna_grupo': 'Tipo',
-            'delimitador': ';',
-            'codificacao': 'utf-8',
-            'special_processing': 'comercio_outros_vinhos'
+        "comercio": {
+            "url": "http://vitibrasil.cnpuv.embrapa.br/download/Comercio.csv",
+            "columns_to_drop": ["control", "id"],
+            "nome_coluna": "Produto",
+            "coluna_grupo": "Tipo",
+            "delimitador": ";",
+            "codificacao": "utf-8",
+            "special_processing": "comercio_outros_vinhos",
         },
-        'importacao': {
-            'url': 'http://vitibrasil.cnpuv.embrapa.br/download/ImpVinhos.csv',
-            'columns_to_drop': [],
-            'nome_coluna': None,
-            'coluna_grupo': None,
-            'delimitador': ';',
-            'codificacao': 'utf-8',
-            'special_processing': 'importacao_exportacao'
+        "importacao": {
+            "url": "http://vitibrasil.cnpuv.embrapa.br/download/ImpVinhos.csv",
+            "columns_to_drop": [],
+            "nome_coluna": None,
+            "coluna_grupo": None,
+            "delimitador": ";",
+            "codificacao": "utf-8",
+            "special_processing": "importacao_exportacao",
         },
-        'exportacao': {
-            'url': 'http://vitibrasil.cnpuv.embrapa.br/download/ExpVinho.csv',
-            'columns_to_drop': [],
-            'nome_coluna': None,
-            'coluna_grupo': None,
-            'delimitador': ';',
-            'codificacao': 'utf-8',
-            'special_processing': 'importacao_exportacao'
-        }
+        "exportacao": {
+            "url": "http://vitibrasil.cnpuv.embrapa.br/download/ExpVinho.csv",
+            "columns_to_drop": [],
+            "nome_coluna": None,
+            "coluna_grupo": None,
+            "delimitador": ";",
+            "codificacao": "utf-8",
+            "special_processing": "importacao_exportacao",
+        },
     }
 
     # Definir o caminho onde os dados serão salvos
-    caminho_saida = './techchallenge/vitivinicultura/data/'
+    caminho_saida = "./techchallenge/vitivinicultura/data/"
 
     for chave, config in datasets.items():
-        config['caminho_saida'] = caminho_saida
+        config["caminho_saida"] = caminho_saida
         try:
             logger.info(f"\nIniciando processamento para: {chave}")
             sanitizador = SanitizadorDataset(config)
             sanitizador.baixar_e_carregar_csv()
             df_sanitizado = sanitizador.sanitizar()
-            sanitizador.salvar_csv(df_sanitizado, f'{chave}_sanitizado.csv')
-            sanitizador.salvar_json(df_sanitizado, f'{chave}_sanitizado.json')
+            sanitizador.salvar_csv(df_sanitizado, f"{chave}_sanitizado.csv")
+            sanitizador.salvar_json(df_sanitizado, f"{chave}_sanitizado.json")
         except Exception as e:
             logger.error(f"Erro ao processar {chave}: {e}")
 
