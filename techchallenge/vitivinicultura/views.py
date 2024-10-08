18ae6007939af2f8ab502c60b4b755f1bbf68f2b
Yuri Braz <ybraz@live.com>
2024-10-08 17:18:15 -0300

    upgrade no views

--- techchallenge/vitivinicultura/views.py
+++ techchallenge/vitivinicultura/views.py
@@ -1,23 +1,50 @@
-from rest_framework import viewsets
-from .models import Producao, Comercio, Processamento, Exportacao, Importacao
-from .serializers import ProducaoSerializer, ComercioSerializer, ProcessamentoSerializer, ExportacaoSerializer, ImportacaoSerializer
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Producao, Comercio, Processamento, Exportacao, Importacao, AnoValor
+from .serializers import (
+    ProducaoSerializer, ComercioSerializer, ProcessamentoSerializer,
+    ExportacaoSerializer, ImportacaoSerializer, AnoValorSerializer
+)
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-class ProducaoViewSet(viewsets.ModelViewSet):
+# Criação de uma classe base para evitar repetição de código
+class BaseModelViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet base para modelos que herdam de BaseModel.
+    """
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['produto', 'tipo']
+    search_fields = ['produto', 'tipo']
+    ordering_fields = ['produto', 'tipo']
+
+class ProducaoViewSet(BaseModelViewSet):
     queryset = Producao.objects.all()
     serializer_class = ProducaoSerializer
 
-class ComercioViewSet(viewsets.ModelViewSet):
+class ComercioViewSet(BaseModelViewSet):
     queryset = Comercio.objects.all()
     serializer_class = ComercioSerializer
 
-class ProcessamentoViewSet(viewsets.ModelViewSet):
+class ProcessamentoViewSet(BaseModelViewSet):
     queryset = Processamento.objects.all()
     serializer_class = ProcessamentoSerializer
 
-class ExportacaoViewSet(viewsets.ModelViewSet):
+class ExportacaoViewSet(BaseModelViewSet):
     queryset = Exportacao.objects.all()
     serializer_class = ExportacaoSerializer
 
-class ImportacaoViewSet(viewsets.ModelViewSet):
+class ImportacaoViewSet(BaseModelViewSet):
     queryset = Importacao.objects.all()
     serializer_class = ImportacaoSerializer
+
+class AnoValorViewSet(viewsets.ModelViewSet):
+    queryset = AnoValor.objects.all()
+    serializer_class = AnoValorSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = [
+        'ano', 'valor', 'producao', 'comercio', 'processamento', 'exportacao', 'importacao'
+    ]
+    search_fields = ['ano', 'valor']
+    ordering_fields = ['ano', 'valor']
