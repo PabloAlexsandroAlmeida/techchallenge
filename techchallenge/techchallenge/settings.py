b7203b5cb665f9df2265ad483730ab72e5598c6f
Pablo Almeida <pabloalmeida@MacBook-Air-de-Pablo.local>
2024-10-31 19:58:14 -0300

    update worflow

--- techchallenge/techchallenge/settings.py
+++ techchallenge/techchallenge/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hme)_)0@csju%i#5ym!&%!m)0f$d))dzk!zqhyek$b!((s29tt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
